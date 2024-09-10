024126f5d28447b0b88c000ba06b47ae1f2428ff
Hemanth-1354 <hemanthkumar_polisetti@srmap.edu.in>
2024-09-10 08:40:36 +0530

    Updating Files

--- app.py
+++ app.py
@@ -1,51 +1,45 @@
 from flask import Flask, render_template, request, redirect, url_for
-from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
 
-# /// = relative path, //// = absolute path
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///db.sqlite'
-app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
-db = SQLAlchemy(app)
+# Mock database - stores tasks with IDs, titles, and completion statuses
+todo_list = [
+    {"id": 1, "title": "Learn Flask", "complete": False},
+    {"id": 2, "title": "Build a To-Do App", "complete": False},
+    {"id": 3, "title": "Deploy app", "complete": False}
+]
 
+# Home page - displays the to-do list
+@app.route('/')
+def index():
+    return render_template('index.html', todo_list=todo_list)
 
-class Todo(db.Model):
-    id = db.Column(db.Integer, primary_key=True)
-    title = db.Column(db.String(100))
-    complete = db.Column(db.Boolean)
-
-
-@app.route("/")
-def home():
-    todo_list = Todo.query.all()
-    return render_template("base.html", todo_list=todo_list)
-
-
-@app.route("/add", methods=["POST"])
+# Add a new task
+@app.route('/add', methods=['POST'])
 def add():
-    title = request.form.get("title")
-    new_todo = Todo(title=title, complete=False)
-    db.session.add(new_todo)
-    db.session.commit()
-    return redirect(url_for("home"))
+    title = request.form.get('title')
+    if title:
+        new_id = len(todo_list) + 1
+        todo_list.append({"id": new_id, "title": title, "complete": False})
+    return redirect(url_for('index'))
 
-
-@app.route("/update/<int:todo_id>")
+# Mark a task as completed or not completed
+@app.route('/update/<int:todo_id>')
 def update(todo_id):
-    todo = Todo.query.filter_by(id=todo_id).first()
-    todo.complete = not todo.complete
-    db.session.commit()
-    return redirect(url_for("home"))
+    for todo in todo_list:
+        if todo["id"] == todo_id:
+            todo["complete"] = not todo["complete"]
+            break
+    return redirect(url_for('index'))
 
-
-@app.route("/delete/<int:todo_id>")
+# Delete a task
+@app.route('/delete/<int:todo_id>')
 def delete(todo_id):
-    todo = Todo.query.filter_by(id=todo_id).first()
-    db.session.delete(todo)
-    db.session.commit()
-    return redirect(url_for("home"))
+    for todo in todo_list:
+        if todo["id"] == todo_id:
+            todo_list.remove(todo)
+            break
+    return redirect(url_for('index'))
 
-if __name__ == "__main__":
-    with app.app_context():
-        db.create_all()
+if __name__ == '__main__':
     app.run(debug=True)
